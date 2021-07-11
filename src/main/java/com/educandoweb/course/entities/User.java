c25f500ee7830554ecff32561599399cc8d2f970
lgusstavo0112 <luizgs@ufba.br>
2021-07-11 17:49:33 -0300

    H2 database, test profile, JPA

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -3,9 +3,17 @@ package com.educandoweb.course.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id //chave primaria do meu BD
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //definie uma chave autoimplementavel no BD
 	private Long id;
 	private String name;
 	private String email;
